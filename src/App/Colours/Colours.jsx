efcf475b859c18bcd73a2de9941a935318c718d1
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-05-15 21:48:33 +1000

    fix(Colours): fixing the format errors

--- src/App/Colours/Colours.jsx
+++ src/App/Colours/Colours.jsx
@@ -8,43 +8,40 @@ const Colours = () => {
     const context = canvas.getContext('2d');
     const size = 256;
     const data = context.getImageData(0, 0, size, 128);
-
-      /* for loop 32,768 times to put data into imagedata */
-      for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            const p = {x: j, y: i};
-            putData(data.data, size, p, dataByXy(size, p));
-        }
+  
+    /* rotate a quadrant */
+    function rotateXy(n, p, r) {
+      if (r.y === 0) {
+          if (r.x === 1) {
+              p.x = n-1 - p.x;
+              p.y = n-1 - p.y;
+          }
+  
+          /* Swap x and y */
+          let t  = p.x;
+          p.x = p.y;
+          p.y = t;
       }
-      
-      /* to get the data by xy */
-      function dataByXy (n, p) {
-        p = {x: p.x, y: p.y};
-        let r = {x: 0, y: 0},
-            s,
-            d=0;
-        for (s=(n/2)|0; s>0; s=(s/2)|0) {
-            r.x = (p.x & s) > 0 ? 1 : 0;
-            r.y = (p.y & s) > 0 ? 1 : 0;
-            d += s * s * ((3 * r.x) ^ r.y);
-            rot(s, p, r);
-        }
-        return d;
     }
-    
-    /* rotate a quadrant appropriately */
-    function rot(n, p, r) {
-        if (r.y === 0) {
-            if (r.x === 1) {
-                p.x = n-1 - p.x;
-                p.y = n-1 - p.y;
-            }
-    
-            /* Swap x and y */
-            let t  = p.x;
-            p.x = p.y;
-            p.y = t;
-        }
+      
+    /* to get the data by xy */
+    function dataByXy (n, p) {
+      p = {x: p.x, y: p.y};
+      let r = {x: 0, y: 0},
+          s,
+          d=0;
+      for (s=(n/2)|0; s>0; s=(s/2)|0) {
+          r.x = (p.x & s) > 0 ? 1 : 0;
+          r.y = (p.y & s) > 0 ? 1 : 0;
+          d += s * s * ((3 * r.x) ^ r.y);
+          rotateXy(s, p, r);
+      }
+      return d;
+    }
+
+    /* To get RGB */
+    function rgbByData(v) {
+      return ((v & 0xf800) << 8) | ((v & 0x7e0) << 5) | ((v & 0x1f) << 3); 
     }
 
     /* To put the data into imagedata */
@@ -59,9 +56,12 @@ const Colours = () => {
       
     }
 
-    /* To get RGB */
-    function rgbByData(v) {
-        return ((v & 0xf800) << 8) | ((v & 0x7e0) << 5) | ((v & 0x1f) << 3); 
+    /* for loop 32,768 times to put data into imagedata */
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+          const p = {x: j, y: i};
+          putData(data.data, size, p, dataByXy(size, p));
+      }
     }
 
     /* To draw the image by imagedata */
