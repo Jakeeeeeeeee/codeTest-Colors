3a75774db47d6fd885780f9beee1cbafe11fc556
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-05-16 14:50:55 +1000

    fix(Canvas): fixing the nimor format erros

--- src/App/Canvas/Canvas.jsx
+++ src/App/Canvas/Canvas.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import useCanvas from '../hooks/useCanvas/useCanvas';
+import useCanvas from './hooks/useCanvas';
 
 const Canvas = () => {
+  const width = 256;
+  const height = 128;
   
   const drawCurve = (ctx) => {
-    const width = 256;
-    const height = 128;
     const data = ctx.getImageData(0, 0, width, height);
   
     /* rotate a quadrant */
-    function rotateXy(n, p, r) {
+    function rotateXY(n, p, r) {
       if (r.y === 0) {
         if (r.x === 1) {
           p.x = n-1 - p.x;
@@ -17,23 +17,23 @@ const Canvas = () => {
         }
 
         /* Swap x and y */
-        const t  = p.x;
+        const t = p.x;
         p.x = p.y;
         p.y = t;
       }
     }
       
     /* to get the data by xy */
-    function dataByXy (n, p) {
+    function dataByXY (n, p) {
       p = {x: p.x, y: p.y};
 
-      let r = {x: 0, y: 0}, s, d=0;
+      let r = {x: 0, y: 0}, s, d = 0;
 
-      for (s=(n/2)|0; s>0; s=(s/2)|0) {
+      for (s = (n/2)|0; s>0; s = (s/2)|0) {
         r.x = (p.x & s) > 0 ? 1 : 0;
         r.y = (p.y & s) > 0 ? 1 : 0;
         d += s * s * ((3 * r.x) ^ r.y);
-        rotateXy(s, p, r);
+        rotateXY(s, p, r);
       }
 
       return d;
@@ -58,9 +58,9 @@ const Canvas = () => {
     /* for loop 32,768 times to put data into imagedata */
     for (let i = 0; i < height; i++) {
       for (let j = 0; j < width; j++) {
-        const p = {x: j, y: i};
+        const p = { x: j, y: i };
 
-        putData(data.data, width, p, dataByXy(width, p));
+        putData(data.data, width, p, dataByXY(width, p));
       }
     }
 
@@ -71,7 +71,7 @@ const Canvas = () => {
   const canvasRef = useCanvas(drawCurve);
 
   return (
-    <canvas ref={canvasRef} width="256px" height="128px"/>
+    <canvas ref={canvasRef} width={width} height={height} />
   );
 }
 
